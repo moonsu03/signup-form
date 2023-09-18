f3eb1648137e79881f0c6380de06d7b3b16fdd6a
R. Y <104298711+moonsuta@users.noreply.github.com>
2023-09-18 10:42:36 +0500

    main section of paragraphs ready!

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+//i don't really need the config
+//just for tailiwindcss extension to work in VSCode
